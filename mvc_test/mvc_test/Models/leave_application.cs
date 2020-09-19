3b11ee17beab7f7d2a6305b6374df44a5b441bf8
薛嘉宁 <292590818@tongji.edu.cn>
2020-09-19 11:11:12 +0800

    Add files via upload

---
+++ mvc_test/mvc_test/Models/leave_application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_test.Models
+{
+    public class leave_application
+    {
+        [Key]
+        public int ask_for_leave_reason { get; set; }
+
+        public int course_id { get; set; }
+
+        public int class_date { get; set; }
+
+        public int s_num { get; set; }
+    }
+}
+
---
+++ mvc_test/mvc_test/Models/supplementary_signature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_test.Models
+{
+    public class supplementary_signature
+    {
+        [Key]
+        public int supply_sign_reason { get; set; }
+        public int supply_sign_sheet_id { get; set; }
+        public int course_id { get; set; }
+
+        public int class_date { get; set; }
+
+        public int s_num { get; set; }
+    }
+}
+
